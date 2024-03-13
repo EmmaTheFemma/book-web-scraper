67eb75391a040b3e66d4aea7a984e0d5468016ea
Albini <90956505+TheDoomfire@users.noreply.github.com>
2024-03-13 22:21:22 +0100

    Improved but still errors

--- main.py
+++ main.py
@@ -60,7 +60,7 @@ def find_url(url):
     print("Starting: find_url()")
     session = HTMLSession()
     r = session.get(url, headers=headers)
-    r.html.render(sleep=2, keep_page=True, scrolldown=1)
+    r.html.render(sleep=4, keep_page=True, scrolldown=1)
 
     #links = r.html.find('a', containing='reminiscences of a stock operator') # works but not getting the first and likely "best" result.
     #print(links[0])
@@ -77,7 +77,15 @@ def find_url(url):
 
     print("Image")
     image = r.html.find('#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.s-wide-grid-style.sg-row > div.sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(7) > div > div > span > div > div > div > div.puisg-col.puisg-col-4-of-12.puisg-col-4-of-16.puisg-col-4-of-20.puisg-col-4-of-24.puis-list-col-left > div > div.s-product-image-container.aok-relative.s-text-center.s-image-overlay-grey.puis-image-overlay-grey.s-padding-left-small.s-padding-right-small.puis-flex-expand-height.puis.puis-v2v5pwx3nl8aar2aoxf0782v1pf > div > span > a > div > img', first=True)
-
+    print("Image: ", image)
+    if not image:
+        print("Didn't find the image.")
+        image = r.html.find('#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.s-wide-grid-style.sg-row > div.sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(6) > div > div > span > div > div > div > div.puisg-col.puisg-col-4-of-12.puisg-col-4-of-16.puisg-col-4-of-20.puisg-col-4-of-24.puis-list-col-left > div > div.s-product-image-container.aok-relative.s-text-center.s-image-overlay-grey.puis-image-overlay-grey.s-padding-left-small.s-padding-right-small.puis-flex-expand-height.puis.puis-v3w24k51po87a424h8xvo4qme6n > div > span > a > div > img', first=True)
+        print("Image: ", image)
+        if not image:
+            print("Still didn't find the image.")
+            image = r.html.find('#search > div.s-desktop-width-max.s-desktop-content.s-opposite-dir.s-wide-grid-style.sg-row > div.sg-col-20-of-24.s-matching-dir.sg-col-16-of-20.sg-col.sg-col-8-of-12.sg-col-12-of-16 > div > span.rush-component.s-latency-cf-section > div.s-main-slot.s-result-list.s-search-results.sg-row > div:nth-child(7) > div > div > span > div > div > div > div.puisg-col.puisg-col-4-of-12.puisg-col-4-of-16.puisg-col-4-of-20.puisg-col-4-of-24.puis-list-col-left > div > div.s-product-image-container.aok-relative.s-text-center.s-image-overlay-grey.puis-image-overlay-grey.s-padding-left-small.s-padding-right-small.puis-flex-expand-height.puis.puis-v3w24k51po87a424h8xvo4qme6n > div > span > a > div > img', first=True)
+            print("Image: ", image)    
     #image = image[0]
 
     all_images = []
@@ -97,7 +105,8 @@ def find_url(url):
                 src_url = srcset_value.split()[0]
                 print(src_url)
                 all_images.append(src_url)
-    print(all_images)
+
+    print("ALL IMAGES: ", all_images)
     link_to_book = first_search_result.absolute_links.pop()
     print("Book URL: ", link_to_book)
 
@@ -118,10 +127,12 @@ def get_pages(url):
     # Need to check this code somehow and see what the response is.
     session = HTMLSession()
     r = session.get(url, headers=headers)
-    r.html.render(sleep=2, keep_page=True, scrolldown=1)
+    r.html.render(sleep=4, keep_page=True, scrolldown=1)
 
     title_element = r.html.find('#productTitle', first=True)
+    print("Title: ", title_element)
     title = title_element.text
+    print("Title: ", title)
 
     author_elements  = r.html.find('span.author.notFaded > a')
     authors = [author.text for author in author_elements ]
@@ -131,8 +142,8 @@ def get_pages(url):
 
 
     # TODO: Add more choises for pages? since seems to be diffrent
+    counter = 0
     while True:
-        print("WHILE TRUE")
         pages = r.html.find('#rpi-attribute-book_details-ebook_pages > div.a-section.a-spacing-none.a-text-center.rpi-attribute-value > span > a > span', first=True)
         if not pages:
             pages = r.html.find('#rpi-attribute-book_details-fiona_pages > div.a-section.a-spacing-none.a-text-center.rpi-attribute-value > span', first=True) # 'NoneType' object has no attribute 'text'
@@ -143,10 +154,13 @@ def get_pages(url):
                 #kindle = r.html.find('#a-autoid-0-announce', first=True) # #a-autoid-4 > a
                 #kindle = r.html.find('#a-autoid-4 > span > a', first=True) # #a-autoid-4 > a
                 kindle = r.html.xpath('//*[@id="a-autoid-4-announce"]')[0] # #a-autoid-4 > a
+                #hello = '//*[@id="a-autoid-4-announce"]'
                 print("KINDLE: ", kindle)
                 kindle_link = kindle.attrs['href']# kindle.absolute_links.pop()
                 kindle_link = "https://www.amazon.com/" + kindle_link
+                #print("Kindle Link: ", kindle_link)
                 r = session.get(kindle_link)
+                r.html.render(sleep=4, keep_page=True, scrolldown=1)
                 continue
         else:
             break
@@ -182,16 +196,18 @@ def main():
     all_names_cleaned = process_lines(all_names)
     #print(all_names_cleaned)
 
-    test_url = "https://www.amazon.com/s?k=the+price+of+time" # Image list index out of range! might be just one?
-    #get_pages(test_url)
-    find_url(test_url)
+    #test_url = "https://www.amazon.com/s?k=the+price+of+time"
+    test_url = "https://www.amazon.com/Pet-Fooled-Dr-Barbara-Royal/dp/B01M3T5JSW/ref=sr_1_2?dib=eyJ2IjoiMSJ9.5DYgxzVq2RRQzN_5fYKBNhl7S_EyulLvG50cRmL7pdGGlwktCrM-ggBdTAicXK3Tphc6V-A8ar34m5Zk1N4klxNYnaSNXGLtHwiBWyou67lrt-5bQG8nby_UYt-qIVAeakIMnPjxjX8V3-fV3sB0BPylfShs14u8-mHEXrI_VQecgNoGWnN-bFvqyXzQRWpuQxJNvorc0doHrqg-4Go4Skfwhe_rhLiGiiNxlYlsaHo.xu0BhXA7d-fTOqxEefAI1XIImyxrVXatBqOJk1sLXng&dib_tag=se&keywords=fooled&qid=1710360558&sr=8-2"
+    #test_url = "https://www.amazon.com/Richer-Wiser-Happier-Greatest-Investors/dp/1501164856/ref=sr_1_1?dib=eyJ2IjoiMSJ9.OLGAola4Qjaw4AlQKC_gzr0MKbQZ_ZYvzvIN50lYIAArqWmAuGX6AaYCyRgV2YTJdv0QIwZWEe6f7_M8Ll-dhUfhWMEFtUBWmOql9QIhpeRRLrczuG0d5ztap0I3TlE0tcL5-20EVPMQK1tkAj3p2sshnA1EIi_R-Rv193w4vAunCLtqo7dfRmmPVtzfrnW5QNhEZqfVdzOTUFk7y_nXHLpXeiu052UvkmrQaRmx_Is.I5F9gT73wCFzlxSfQ-A2KBkirQXQC5SKLnHstLfumBk&dib_tag=se&keywords=richer+wiser+happier&qid=1710360294&sr=8-1"
+    #find_url(test_url)
+    get_pages(test_url)
     temp_count = 0 # 0 = off, anything else = on
 
     markdown_content = "--- ---\n\n"
     if temp_count == 1: # So i can turn it off while testing.
         for book in all_names_cleaned:
                 print("BOOK: ", book)
-            #if temp_count < 1:
+            #if temp_count < 1: 
                 url = amazon_url + book
                 #print(url)
                 result_one = find_url(url) #AttributeError: 'NoneType' object has no attribute 'text'
@@ -212,8 +228,9 @@ def main():
                 #temp_count += 1
                 print(markdown_content)
 
-                print("Waiting 10 seconds for next")
-                time.sleep(10) # in seconds
+                seconds_to_wait = 240
+                print("Waiting " + str(seconds_to_wait) + " seconds for next")
+                time.sleep(seconds_to_wait) # in seconds
 
     with open(markdown_file, 'w') as file: # Write the entire Markdown content to the file
         file.write(markdown_content)
